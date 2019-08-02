99322318506ed83b437226ad5918f92764615c33
Derek Wu <derekwu35@gmail.com>
2019-08-02 13:44:23 -0500

    initial commit for ionic version

--- index.js
+++
@@ -1,7 +0,0 @@
-//Entry Point for App
-//Root element for Index.html
-import { AppRegistry } from 'react-native'; //App
-import App from './src/App';
-
-AppRegistry.registerComponent('qrdr', () => App); //App
-//ReactDom.render(<App />, document.getElementById('root')); //Web
--- src/index.js
+++
@@ -1,9 +0,0 @@
-//Entry Point for App
-//Root element for Index.html
-import React from 'react'; //;Web
-import ReactDom from 'react-dom'; //Web
-import { AppRegistry } from 'react-native'; //App
-import App from './App';
-
-AppRegistry.registerComponent('qrdr', () => App); //App
-ReactDom.render(<App />, document.getElementById('root')); //Web
