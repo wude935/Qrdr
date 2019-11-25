4eed35c0b75dd05657ac56cdc4df1236549fdee4
Derek Wu <derekwu35@gmail.com>
2019-11-24 18:26:16 -0600

    hosting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartPageModule } from './pages/cart/cart.module';
 import { CartService } from './services/cart.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,7 +21,8 @@ import { CartService } from './services/cart.service';
     IonicModule.forRoot(),
     IonicStorageModule.forRoot(),
     AppRoutingModule,
-    CartPageModule
+    CartPageModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     StatusBar,
